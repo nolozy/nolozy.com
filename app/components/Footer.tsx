76c6553a1e64b0ae4bf780cbfbdef9bc75d75f2f
nolozy <nolozyofficial@gmail.com>
2023-08-11 23:54:28 +0900

    title and footer fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="mt-6 text-center text-xs text-white">
+    <footer className="mt-6 text-center text-xs text-gray-900 dark:text-white">
       <p>© 2023 nolozy.com</p>
     </footer>
   )
